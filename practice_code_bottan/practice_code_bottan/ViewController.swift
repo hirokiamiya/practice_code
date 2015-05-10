18ab195da08961a6b72801ca50a5caf3114acf24
Hiroki Amiya <kisarazu1862@gmail.com>
2015-05-10 18:46:31 +0900

    first time

---
+++ practice_code_bottan/practice_code_bottan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice_code_bottan
+//
+//  Created by akimiya on 2015/05/10.
+//  Copyright (c) 2015年 akimiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
